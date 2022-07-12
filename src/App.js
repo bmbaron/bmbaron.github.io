a0f32f166c69d048e3f5c105a93895e7591307de
bmbaron <benjamin.m.baron@gmail.com>
2022-07-12 16:27:19 +0700

    added react helmet and fixed some small type

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import './App.css';
 import Header from './components/Header'
 import Body from './components/Body'
 import Footer from './components/Footer'
+import {Helmet} from 'react-helmet'
 
 function App() {
   return (
     <>
+      <Helmet>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0" />
+          <title>Ben Baron</title>
+          <link rel="icon" href="./images/favicon.ico" />
+          <link rel="canonical" href="http://bmbaron.github.io" />
+      </Helmet>
       <Header />
       <Body />
       <Footer />
